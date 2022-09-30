6029ec9be58787ff8d4caeb9a9fa9bd0ad16f70b
SumiAkter2 <akteralisha36@gmail.com>
2022-09-30 18:47:56 +0600

    banner content set up

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,25 +1,78 @@
 import React from "react";
-
+import "./Banner.css";
 const Banner = () => {
   return (
     <div>
-      <div
-        className="hero min-h-screen"
-        style={{
-          backgroundImage: `url("https://i.ibb.co/jhCzQzX/Rectangle-1.png")`,
-        }}
-      >
-        <div className="hero-overlay bg-opacity-10"></div>
-        <div className="hero-content text-center text-neutral-content">
-          <div className="max-w-md">
-            <img src="https://i.ibb.co/wswNXN2/Line-1.png" alt="" />
-            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
-            <p className="mb-5">
-              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
-              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
-              et a id nisi.
-            </p>
-            <button className="btn btn-primary">Get Started</button>
+      <div>
+        {/* ********************banner content*************** */}
+        <div
+          className="hero banner-img "
+          style={{
+            backgroundImage: `url("https://i.ibb.co/jhCzQzX/Rectangle-1.png")`,
+            height: "799px",
+          }}
+        >
+          <div className="  text-white ">
+            {/*--------------- banner-content ---------------*/}
+            <div className="  banner-content ">
+              <div className="flex justify-between items-center banner-left-content ">
+                <div className=" w-1/2">
+                  {/* -----------top-content--------- */}
+                  <div className=" top-horizontal-line ">
+                    <img
+                      src="https://i.ibb.co/wswNXN2/Line-1.png"
+                      alt="line-img"
+                    />
+                    <p className="top-content">A tradition since 2002</p>
+                  </div>
+                  {/*--------------- banner-left-content ---------------*/}
+                  <h1>
+                    Offering Bright <br /> <span>Futures</span>
+                  </h1>
+                  <p>
+                    iNiLabs school graduates are studding in the world
+                    <span>most prestigious universities</span>
+                  </p>
+                  <button className="apply-btn">Apply Now</button>
+                </div>
+                {/* ----------------banner-right-content------ */}
+                <div className="lg:flex  justify-center items-start w-1/2 banner-right-content">
+                  <button className="circle-btn">
+                    <img
+                      src="https://i.ibb.co/Mpcg2X1/Ellipse-1.png"
+                      alt="btn-logo"
+                    />
+                  </button>
+                  <div className="grid ml-2">
+                    <h4>iNiLabs School</h4>
+                    <h3 className="text-xl font-bold">
+                      CAMPUS <br />
+                      <span>TOUR</span>
+                    </h3>
+                    <p className="text-sm">WATCH VIDEO (6 MIN)</p>
+                  </div>
+                </div>
+              </div>
+            </div>
+          </div>
+
+          {/* -----------horizontal-line for bottom------------------- */}
+          <div className="flex justify-center items-center  horizontal-line">
+            <img
+              className="mr-4"
+              src="https://i.ibb.co/wswNXN2/Line-1.png"
+              alt="line-img"
+            />
+            <img
+              className="mr-4"
+              src="https://i.ibb.co/KxY7Hmm/Line-2.png"
+              alt="line-img"
+            />
+            <img
+              className="mr-4 "
+              src="https://i.ibb.co/wswNXN2/Line-1.png"
+              alt="line-img"
+            />
           </div>
         </div>
       </div>
