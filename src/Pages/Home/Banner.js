32f1771ca1b36403e7862b1db675cefecdd86e79
SumiAkter2 <akteralisha36@gmail.com>
2022-09-30 19:31:43 +0600

    added card item after banner content

--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -76,6 +76,54 @@ const Banner = () => {
           </div>
         </div>
       </div>
+
+      {/* --------after banner card content-------------- */}
+      <div className="grid  md:grid-cols-3 lg:grid-cols-4 w-full">
+        <div className="card-1">
+          <img src="https://i.ibb.co/rm3xyz9/Compound-Path.png" alt="icon" />
+          <h1>
+            Educational in English:
+            <br />
+            <span>
+              Cambridge international programme and qualified native english
+              speaking teachers
+            </span>
+          </h1>
+        </div>
+        <div className="card-2">
+          <img src="https://i.ibb.co/0GPk0QF/Vector.png" alt="icon" />
+          <h1>
+            Educational in English:
+            <br />
+            <span>
+              Cambridge international programme and qualified native english
+              speaking teachers
+            </span>
+          </h1>
+        </div>
+        <div className="card-1">
+          <img src="https://i.ibb.co/qDFfdKG/Vector-2.png" alt="icon" />
+          <h1>
+            Educational in English:
+            <br />
+            <span>
+              Cambridge international programme and qualified native english
+              speaking teachers
+            </span>
+          </h1>
+        </div>
+        <div className="card-2">
+          <img src="https://i.ibb.co/nn4z2gN/Vector-2.png" alt="icon" />
+          <h1>
+            Educational in English:
+            <br />
+            <span>
+              Cambridge international programme and qualified native english
+              speaking teachers
+            </span>
+          </h1>
+        </div>
+      </div>
     </div>
   );
 };
