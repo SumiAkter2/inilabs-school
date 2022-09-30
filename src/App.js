6032977b282dcbdd51a0df2b4f92483073137343
SumiAkter2 <akteralisha36@gmail.com>
2022-09-30 16:35:26 +0600

    added hero content to banner component

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
 import Header from "./Pages/Header/Header";
+import Banner from "./Pages/Home/Banner";
 
 function App() {
   return (
     <div>
       <Header />
-      <h1>Hello iNiLabs school</h1>
+      <Banner />
     </div>
   );
 }
--- src/Pages/Home/Banner.js
+++ src/Pages/Home/Banner.js
@@ -1,11 +1,30 @@
-import React from 'react';
+import React from "react";
 
 const Banner = () => {
-    return (
-        <div>
-            
+  return (
+    <div>
+      <div
+        className="hero min-h-screen"
+        style={{
+          backgroundImage: `url("https://i.ibb.co/jhCzQzX/Rectangle-1.png")`,
+        }}
+      >
+        <div className="hero-overlay bg-opacity-10"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <img src="https://i.ibb.co/wswNXN2/Line-1.png" alt="" />
+            <h1 className="mb-5 text-5xl font-bold">Hello there</h1>
+            <p className="mb-5">
+              Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda
+              excepturi exercitationem quasi. In deleniti eaque aut repudiandae
+              et a id nisi.
+            </p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
         </div>
-    );
+      </div>
+    </div>
+  );
 };
 
 export default Banner;
