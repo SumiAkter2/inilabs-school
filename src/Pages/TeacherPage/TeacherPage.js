5419e267e253a0757f4d4a43eb8675c17885d17a
SumiAkter2 <akteralisha36@gmail.com>
2022-10-01 11:57:14 +0600

    added social media to img

--- src/Pages/TeacherPage/TeacherPage.js
+++ src/Pages/TeacherPage/TeacherPage.js
@@ -4,18 +4,17 @@ const TeacherPage = () => {
   return (
     <div>
       <div className="teacher-container">
+        {/* -----------line-icon------------ */}
         <img src="https://i.ibb.co/KxY7Hmm/Line-2.png" alt="icon" />
         <h1>Our Teacher</h1>
-        <img
-          className="arrow-1"
-          src="https://i.ibb.co/s5sGVdD/Vector-5.png"
-          alt="icon"
-        />
-        <img
-          className="arrow-2"
-          src="https://i.ibb.co/wBHVbdj/Vector-4.png"
-          alt="icon"
-        />
+        {/*------------ right site arrow ------------*/}
+        <button className="arrow-1 text-5xl">
+          <img src="https://i.ibb.co/s5sGVdD/Vector-5.png" alt="icon" />
+        </button>
+        <button className="arrow-2">
+          <img src="https://i.ibb.co/wBHVbdj/Vector-4.png" alt="icon" />
+        </button>
+        {/* -----------teacher card----------------- */}
       </div>
       <div className="teacher-content flex">
         <div className="teacher-1">
@@ -26,13 +25,8 @@ const TeacherPage = () => {
           <h1>Cooper Donin</h1>
           <p>Head of Operation</p>
         </div>
-        <div className="social-logo bg-red-500  ">
-          <p>ygioygiygi</p>
-          <img src="https://i.ibb.co/zGwtkNd/Vector-3.png" alt="social-icon" />
-          <img src="https://i.ibb.co/FwFPwv4/Vector-7.png" alt="social-icon" />
-          <img src="https://i.ibb.co/QPsFDH4/Vector-5.png" alt="social-icon" />
-          <img src="https://i.ibb.co/TBTFdth/Vector-6.png" alt="social-icon" />
-        </div>
+        {/* ------------------social media------------------- */}
+
         <div className="teacher-2">
           <img
             src="https://i.ibb.co/pZbnHnP/Rectangle-24.png"
@@ -41,6 +35,40 @@ const TeacherPage = () => {
           <h1>Anika Baptista</h1>
           <p>Arts Teacher</p>
         </div>
+        <div className="social-logo bg-red-500  ">
+          {/* ----------------facebook----------- */}
+          <div className="facebook">
+            <img
+              src="https://i.ibb.co/zGwtkNd/Vector-3.png"
+              alt="social-icon"
+            />
+          </div>
+
+          <div>
+            <svg
+              className=" twitter"
+              xmlns="http://www.w3.org/2000/svg"
+              viewBox="0 0 448 512"
+              width="48"
+              fill="#FFFFFF"
+            >
+              <path d="M400 32H48C21.5 32 0 53.5 0 80v352c0 26.5 21.5 48 48 48h352c26.5 0 48-21.5 48-48V80c0-26.5-21.5-48-48-48zm-48.9 158.8c.2 2.8.2 5.7.2 8.5 0 86.7-66 186.6-186.6 186.6-37.2 0-71.7-10.8-100.7-29.4 5.3.6 10.4.8 15.8.8 30.7 0 58.9-10.4 81.4-28-28.8-.6-53-19.5-61.3-45.5 10.1 1.5 19.2 1.5 29.6-1.2-30-6.1-52.5-32.5-52.5-64.4v-.8c8.7 4.9 18.9 7.9 29.6 8.3a65.447 65.447 0 0 1-29.2-54.6c0-12.2 3.2-23.4 8.9-33.1 32.3 39.8 80.8 65.8 135.2 68.6-9.3-44.5 24-80.6 64-80.6 18.9 0 35.9 7.9 47.9 20.7 14.8-2.8 29-8.3 41.6-15.8-4.9 15.2-15.2 28-28.8 36.1 13.2-1.4 26-5.1 37.8-10.2-8.9 13.1-20.1 24.7-32.9 34z" />
+            </svg>
+          </div>
+          <div className="linkedin">
+            <img
+              src="https://i.ibb.co/QPsFDH4/Vector-5.png"
+              alt="social-icon"
+            />
+          </div>
+          <div className="google">
+            <img
+              src="https://i.ibb.co/TBTFdth/Vector-6.png"
+              alt="social-icon"
+            />
+          </div>
+        </div>
+
         <div className="teacher-3">
           <img
             src="https://i.ibb.co/r7BxSRb/Rectangle-25.png"
@@ -50,9 +78,14 @@ const TeacherPage = () => {
           <p>Spanish Teacher</p>
         </div>
       </div>
+      {/* -----------------------see teacher btn --------------- */}
       <div className="see-teacher-content flex">
         <p>See Teacher</p>
-        <img src="https://i.ibb.co/wBHVbdj/Vector-4.png" alt="icon" />
+        <img
+          className="left-arrow"
+          src="https://i.ibb.co/wBHVbdj/Vector-4.png"
+          alt=""
+        />
       </div>
     </div>
   );
